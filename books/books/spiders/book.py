1fcd78464bdfd8a4cabaa0b69595dbfae5c1261d
azzouzioussama <azzouzioussama@gmail.com>
2025-04-11 23:55:21 +0200

    implement parsing logic in BookSpider to extract book details

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from books.items import BooksItem
 
 class BookSpider(scrapy.Spider):
     name = "book"
@@ -7,4 +7,10 @@ class BookSpider(scrapy.Spider):
     start_urls = ["http://books.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        for book in response.css("article.product_pod"):
+            item = BooksItem()
+            item["url"] = book.css("h3 > a::attr(href)").get()
+            item["title"] = book.css("h3 > a::attr(title)").get()
+            item["price"] = book.css("p.price_color::text").get()
+            yield item
+

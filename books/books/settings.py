f47265905cfea90f7919520d1ee474ea9196941a
azzouzioussama <azzouzioussama@gmail.com>
2025-04-15 19:17:43 +0200

    feat: add MongoDB connection settings for local database

--- books/books/settings.py
+++ books/books/settings.py
@@ -92,3 +92,5 @@ REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
+MONGO_URI = "mongodb://localhost:27017"
+MONGO_DATABASE = "books_db"
